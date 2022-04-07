ab776e80a6eaf85b3c6169e72e1f28958a3a942d
Andrew Botham <andrew_botham@hotmail.co.uk>
2022-04-07 13:59:34 +0100

    third draft

--- app.js
+++ app.js
@@ -1,99 +1,8 @@
 
-// const foodButton = document.querySelector("#foodButton");
-// const drinkButton = document.querySelector("#drinkButton");
-// const playButton = document.querySelector("#playButton");
-
-// const petUpdate = document.querySelector(".petUpdate");
-// // button id listeners
-// foodButton.addEventListener("click", () => {
-//   newDog.eat();
-  
-// });
-
-// drinkButton.addEventListener("click", () => {
-//   newDog.drink();
-  
-// });
-
-// playButton.addEventListener("click", () => {
-//   newDog.play();
-  
-// });
-
-
-// //universal pet class
-// class Animal {
-//   constructor(name){
-//   this._name = name;
-//   this._hunger=100;
-//   this._thirst=50;
-//   this._happy=50;
-// }
-// get name(){
-//   return this._name;
-// }
-// get hunger(){
-//   return this._hunger;
-// }
-// get thirst(){
-//   return this._thirst;
-// }
-// get happy(){
-//   return this._happy;
-// }
-
-// eat(){
-//     this._hunger-=10;
-//     this._thirst+=5;
-//     this._happy+=5
-//   if (this._hunger >= 90) {
-//     // Link to DOM element ID here
-//     petUpdate.textContent = `${this._name} wants scooby snacks... FEED ME!`;
-//   } else if(this._hunger <= 10) {
-//     // Link to DOM element ID here
-//     petUpdate.textContent = `${this._name} is going to burst! No more food`;
-//   } else {
-//     // Link to DOM element ID here - maybe swap dog image
-//     petUpdate.textContent = `${this._hunger} hunger level` ;
-//   };
-// };
-
-// drink(){
-//     this._thirst-=10;
-//     this._hunger+=5;
-//     this._happy+=5;
-//   if (this._thirst >= 90) {
-//     // Link to DOM element ID here
-//     petUpdate.textContent = `${this._name} is SO thirsty, drinkies please!`;
-//   } else if(this._thirst <= 10) {
-//     // Link to DOM element ID here
-//     petUpdate.textContent = `${this._name} is not thirsty anymore`;
-//   } else {
-//     // Link to DOM element ID here - maybe swap dog image
-//     petUpdate.textContent = `${this._thirst} thirst level` ;
-//   };
-// };
-
-// play(){
-//     this._happy-=10;
-//     this._hunger+=5;
-//     this._thirst=+5;
-//   if (this._happy >= 90) {
-//     // Link to DOM element ID here
-//     petUpdate.textContent = `${this._name} is bored. I need to play!`;
-//   } else if(this._happy <= 10) {
-//     // Link to DOM element ID here
-//     petUpdate.textContent = `${this._name} is tired out. I need to rest`;
-//   } else {
-//     // Link to DOM element ID here - maybe swap dog image
-//     petUpdate.textContent = `${this._happy} play level` ;
-//   };
-// };
-
-// };
-
-// const newDog = new Animal ("layla")
 const petUpdate = document.getElementById('petUpdate');
+const petHunger = document.getElementById('petHunger');
+const petThirst = document.getElementById('petThirst');
+const petHappy = document.getElementById('petHappy');
 
 const foodButton = document.getElementById("feedButton");
 const drinkButton = document.getElementById("drinkButton");
@@ -102,19 +11,21 @@ const playButton = document.getElementById("playButton");
 
 // button id listeners
 foodButton.addEventListener("click", (event) => {
-  petUpdate.innerHTML = ` wants scooby snacks... FEED ME!`;
   newDog.eat();
-  
+  newDog.checkHunger();
+  newDog.displayScore();
 });
 
 drinkButton.addEventListener("click", (event) => {
   newDog.drink();
-  
+  newDog.checkThirst();
+  newDog.displayScore();
 });
 
 playButton.addEventListener("click", (event) => {
   newDog.play();
-  
+  newDog.checkHappy();
+  newDog.displayScore();
 });
 
 
@@ -122,68 +33,81 @@ playButton.addEventListener("click", (event) => {
 class Animal {
   constructor(name){
   this._name = name;
-  this._hunger=100;
-  this._thirst=50;
-  this._happy=50;
+  this._hunger = 50;
+  this._thirst = 50;
+  this._happy = 50;
 }
 get name(){
-  return this._name;
+  return this._name
 }
 get hunger(){
-  return this._hunger;
+  return this._hunger
 }
 get thirst(){
-  return this._thirst;
+  return this._thirst
 }
 get happy(){
-  return this._happy;
+  return this._happy
 }
 
 eat(){
-    this._hunger-=10;
-    this._thirst+=5;
-    this._happy+=5
-  if (this._hunger >= 90) {
-    // Link to DOM element ID here
-    petUpdate.textContent = `${this._name} wants scooby snacks... FEED ME!`;
-    // console.log(`${this._name} wants scooby snacks... FEED ME!`);
-  } else if(this._hunger <= 10) {
-    // Link to DOM element ID here
-    petUpdate.textContent = (`${this._name} is going to burst! No more food`);
-  } else {
-    // Link to DOM element ID here - maybe swap dog image
-    petUpdate.textContent = (`${this._hunger} hunger level`) ;
-  };
-};
+  this._hunger -= 10;
+  this._thirst += 5;
+  this._happy += 5;
+}
+
 drink(){
-    this._thirst-=10;
-    this._hunger+=5;
-    this._happy+=5;
-  if (this._thirst >= 90) {
-    // Link to DOM element ID here
-    petUpdate.textContent = (`${this._name} is SO thirsty, drinkies please!`);
-  } else if(this._thirst <= 10) {
-    // Link to DOM element ID here
-    petUpdate.textContent = (`${this._name} is not thirsty anymore`);
-  } else {
-    // Link to DOM element ID here - maybe swap dog image
-    petUpdate.textContent = (`${this._thirst} thirst level`) ;
-  };
-};
+  this._thirst -= 10;
+  this._hunger += 5;
+  this._happy += 5;
+}
+
 play(){
-  this._happy-=10;
-  this._hunger+=5;
-  this._thirst=+5;
-  if (this._happy >= 90) {
-    // Link to DOM element ID here
-    petUpdate.textContent = (`${this._name} is bored. I need to play!`);
-  } else if(this._happy <= 10) {
-    // Link to DOM element ID here
-    petUpdate.textContent = (`${this._name} is tired out. I need to rest`);
+  this._happy -= 10;
+  this._hunger += 5;
+  this._thirst += 5;
+}
+
+
+displayScore(){
+  petHunger.innerHTML = `Hunger ${this._hunger}.`; 
+  petThirst.innerHTML = `Thirst ${this._thirst}.`; 
+  petHappy.innerHTML = `Happy ${this._happy}`;
+}
+
+checkHunger(){
+  if (this._hunger >= 90) {
+    petUpdate.innerHTML = `${this._name} wants scooby snacks... FEED ME!`;
+  } else if(this._hunger <= 10) {
+    petUpdate.innerHTML = `${this._name} is going to burst! No more food`;
   } else {
     // Link to DOM element ID here - maybe swap dog image
-    petUpdate.textContent = (`${this._happy} play level`) ;
+    petUpdate.innerHTML = `${this._hunger} hunger level`;
   };
 };
+
+checkThirst(){
+  if (this._thirst >= 90) {
+    petUpdate.innerHTML = `${this._name} is SO thirsty, drinkies please!`;
+  } else if(this._thirst <= 10) {
+    petUpdate.innerHTML = `${this._name} is not thirsty anymore`;
+  } else {
+    // Link to DOM element ID here - maybe swap dog image
+    petUpdate.innerHTML = `${this._thirst} thirst level`;
+  };
 };
+
+checkHappy(){
+  if (this._happy >= 90) {
+    petUpdate.innerHTML = `${this._name} is bored. I need to play!`;
+  } else if(this._happy <= 10) {
+    petUpdate.innerHTML = `${this._name} is tired out. I need to rest`;
+  } else {
+    // Link to DOM element ID here - maybe swap dog image
+    petUpdate.innerHTML = `${this._happy} happy level`;
+  };
+};
+
+};
+
 const newDog = new Animal("Layla");
